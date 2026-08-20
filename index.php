25b1c8c7aeac11f9730f6f8080bc8baa75b82a7c
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-20 19:29:14 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Dentro del head van las configuraciones de nuestra pagina para con el navegador -->
+    <meta charset="UTF-8"><!-- define que codificacion de caracteres se utilizara en la pagina -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primera clase PHP</title><!-- Titulo de la pagina (El que muestra el navegador) -->
+</head>
+<body>
+
+    <?php
+        echo "Este es un script de PHP";
+        $nota = 4;
+
+        if ($nota >= 6) {
+            echo "Aprobado";
+        } else {
+            echo "Desaprobado";
+        }
+    ?>
+    <!-- Dentro del body va el contenido de la pagina -->
+    <h1>Hola visitante!</h1>
+    <p>Esta es mi primera pagina web</p>
+    <input/>
+</body>
+</html>
+
+
+
